8ab4d6767d5b57a2bb3c037e450bc568bfbf586d
Anmol Verma <anmolverma102002@gmail.com>
2022-04-24 22:12:24 +0530

    21 04 2022

---
+++ 2022_04_21.py
@@ -0,0 +1,69 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+# setting up connection
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("succesful")
+    except Error as err:
+        print("Error: '{err}'")
+    return connection
+
+# put our MYSQL terminal passwrod
+
+
+pw = "Anmol@mysql"
+
+# Database name
+db = "world"
+connection = create_server_connection("localhost", "root", pw)
+
+# create my_sql python
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("database cerated successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+    create_database_query = "Create database python"
+    create_database(connection, create_database_query)
+
+
+# connect to database
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name)
+        print("Mysql database connction successfull")
+    except Error as err:
+        print("Error: '{err}'")
+    return connection
+
+
+# execute sql queries
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("success")
+    except Error as err:
+        print("Error: '{err}'")
