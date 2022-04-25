26b7c2c0f3f1495fc4f070c97c8d6b9543554151
Anmol Verma <anmolverma102002@gmail.com>
2022-04-25 15:19:42 +0530

    Removed unwanted file

--- 00dR.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-from tkinter import messagebox
-root = tkinter.Tk()
-root.withdraw()
-
-messagebox.showerror("Error","Error message")
-messagebox.showwarning("Warning","Warning message")
-messagebox.showinfo("Information","Information message")
